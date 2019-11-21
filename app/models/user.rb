271eb2824c5b0f4cbe2d6c39af230d8d4ead78e2
Charlotte Goldthwaite <chargold201@gmail.com>
2019-11-21 11:40:26 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    if happiness && nausea
+      if happiness >= nausea
+        "happy"
+      else
+        "sad"
+      end
+    else
+      "unknown"
+    end
+  end
 end
